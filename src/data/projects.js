b90350062db08d699b900aa9e72fdcc33a2a660a
Arthur Gassner <arth.gassner@gmail.com>
2022-03-06 21:32:03 +0100

    Update projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -10,6 +10,17 @@ const data = [
       'A joke on the popular word-guessing game that too the world by '
       + 'storm in February 2022: wordle. It\'s absurd and made me laugh.',
   },
+  {
+    title: 'OpenCSI: An Open-Source Dataset for Indoor Localization Using CSI-Based Fingerprinting',
+    subtitle: 'The open-sourcing of a dataset used for indoor localization using the LTE network',
+    link: 'https://arxiv.org/abs/2104.07963',
+    image: '/images/projects/openCSI.png',
+    date: '2021-04-16',
+    desc:
+      'Using information extracted from the wireless signal of an LTE tower, one can try to estimate '
+      + 'its position. This paper open-sources the first dataset of this kind using '
+      + 'the Channel State Information (CSI) to do just that.',
+  },
 ];
 
 export default data;
