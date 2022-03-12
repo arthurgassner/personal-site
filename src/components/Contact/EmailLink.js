fb3a6145e2ec4044fa961f88b00735f499bc2509
Arthur Gassner <arth.gassner@gmail.com>
2022-03-12 19:59:15 +0100

    Replace name by Arthur Gassner

--- src/components/Contact/EmailLink.js
+++ src/components/Contact/EmailLink.js
@@ -66,7 +66,7 @@ const EmailLink = () => {
       onMouseEnter={() => setIsActive(false)}
       onMouseLeave={() => (idx < messages.length) && setIsActive(true)}
     >
-      <a href={validateText(message) ? `mailto:${message}@mldangelo.com` : ''}>
+      <a href={validateText(message) ? `mailto:${message}@gmail.com` : ''}>
         <span>{message}</span>
         <span>@gmail.com</span>
       </a>
