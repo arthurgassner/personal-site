f563035496f865af7a46ecd6486914b28bc81a85
Arthur Gassner <arth.gassner@gmail.com>
2023-06-27 09:48:03 +0200

    Update projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,15 +1,4 @@
-// TODO Add OpenCSI paper and some school projects
 const data = [
-  {
-    title: 'Dumble.ch',
-    subtitle: 'A 1-letter version of the popular word-guessing game wordle',
-    link: 'http://www.dumble.ch',
-    image: '/images/projects/dumble.png',
-    date: '2022-02-25',
-    desc:
-      'A joke on the popular word-guessing game that too the world by '
-      + 'storm in February 2022: wordle. It\'s absurd and made me laugh.',
-  },
   {
     title: 'OpenCSI: An Open-Source Dataset for Indoor Localization Using CSI-Based Fingerprinting',
     subtitle: 'The open-sourcing of a dataset used for indoor localization using the LTE network',
