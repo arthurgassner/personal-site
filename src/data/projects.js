800c9350e368d9fa57ae11495e9b6ce3bc471955
Arthur Gassner <arth.gassner@gmail.com>
2025-08-03 19:45:40 +0200

    Add literature clock

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,16 @@
 const data = [
   {
+    title: 'Literature Clock | Literature clock from scratch',
+    subtitle: 'Building a literature clock with a Raspberry Pi, an e-ink screen and Python.',
+    link: 'https://literature-clock.arthurgassner.ch',
+    image: '/images/projects/literature-clock.png',
+    date: '2024-11-17',
+    desc:
+      'I built a literature clock, randomlyy selecting a literary quote and displaying it on an e-ink screen.'
+      + 'The software is written in Python, powered by a Raspberry Pi Zero 2W, and the enclosing is 3D printed, '
+      + 'Both the frontend and backend are open-sourced.',
+  },
+  {
     title: 'Swiss Energy Forecast | end-to-end ML solution',
     subtitle: 'Showcasing an end-to-end machine learning project in the energy industry.',
     link: 'https://swissenergyforecast.com',
